9824bd777aea6d7e4aca3f67bfae5233d894ae3c
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2025-09-04 15:02:44 +0900

    feat: port 변경

--- backend/main.py
+++ backend/main.py
@@ -233,4 +233,6 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 10000))  # Render가 주는 PORT 사용
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
